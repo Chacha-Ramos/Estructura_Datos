c77c73bc19b33d21493dfdb1d36d67c4146d2808
salvadortranquilino-ramos <chacha.ramos.2001@gmail.com>
2022-04-22 06:41:11 -0500

    Project 7 Done

---
+++ Project 7/src/main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by chacha ramos on 4/16/22.
+//
+
+int main() {
+    return 0;
+}
---
+++ Project 7/tst/testing.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Salvador Tranquilino-Ramos on 4/20/22.
+//
+
+#include <gtest/gtest.h>
+#include <graph.h>
+
+TEST(case1, graph) {
+    graph<int, int> G;
+}
